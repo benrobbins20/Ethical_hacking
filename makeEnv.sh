831348cc32f389390b3e29e69075efdd32dcbddd
benQEMUKali <benro2211@gmail.com>
2022-05-01 13:17:23 -0600

    update env install

--- makeEnv.sh
+++ makeEnv.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/sh
-git clone https://github.com/benrobbins20/Ethical_hacking.git
-python3 -m venv Ethical_hacking/Ethical_hacking_venv
-source Ethical_hacking/Ethical_hacking_venv/bin/activate
+python3 -m venv Ethical_hacking_venv
+source Ethical_hacking_venv/bin/activate
 apt update 
 apt install build-essential libnetfilter-queue-dev
 pip3 install -r requirements.txt
