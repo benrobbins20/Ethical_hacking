02b5f050ce8c78625ff8098241b73698378dbd09
benrobbins20 <72369539+benrobbins20@users.noreply.github.com>
2022-05-01 13:32:09 -0600

    Delete makeEnv.sh

--- makeEnv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-python3 -m venv Ethical_hacking_venv
-source Ethical_hacking_venv/bin/activate
-apt update 
-apt install build-essential libnetfilter-queue-dev
-pip3 install -r requirements.txt
